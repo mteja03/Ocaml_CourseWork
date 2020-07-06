4b4a3d23497229bd1be3843c4e3e5b926c686648
mteja03 <62012634+mteja03@users.noreply.github.com>
2020-07-06 20:47:45 +0530

    Add files via upload

---
+++ sequence-arithmetic/_build/default/lib/SequenceArithmetic.ml
@@ -0,0 +1,23 @@
+
+(* type declaration for intseq *)
+
+type intseq = int list;;
+
+(* implementations *)
+let mInt = fun x y -> x * y;;
+(* function that adds two sequences from head to tail, summing at each
+   position. *)
+let rec seqadd : intseq -> intseq -> intseq =
+  fun xs ys -> match xs, ys with [], [] -> []
+  |[x], [y] -> [(x+y)]
+  | h1::rest1 , h2 :: rest2 -> (h1+h2) :: seqadd rest1 rest2
+  |xs, [] -> []
+  | [], ys -> [] ;;
+
+  
+(* function that multiplies two lists from head to tail, multiplying
+   at each position *)
+let rec seqmult : intseq -> intseq -> intseq =
+  fun xs ys -> match xs, ys with [], [] -> []
+  |[x], [y] -> [(mInt x y)]
+  | h1::rest1, h2 :: rest2 -> (mInt h1 h2) :: seqmult rest1 rest2 ;;
